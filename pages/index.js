630a33b5dba21419c48f0b40291b99002931997d
Jspates24 <jonahspates4@gmail.com>
2025-05-28 04:49:16 -0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+/// FILE: pages/index.js
+export default function Home() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Card Value Tracker</h1>
+      <p>Welcome to your private Pokémon and sports card tracker.</p>
+    </div>
+  );
+}
